4195b3f4436f8b4335eff6864c6305a7d17c7b5c
PSURI1894 <parthsuri009@gmail.com>
2026-05-11 10:35:00 +0530

    Refine and optimize implementation of triton-client-go codebase

--- fraud-service/triton_client.go
+++ fraud-service/triton_client.go
@@ -1,16 +1,1 @@
-package main
-
-import (
-	"context"
-	pb "github.com/payments/fraud-service/proto"
-)
-
-type TritonClient struct{}
-
-func NewTritonClient() *TritonClient {
-	return &TritonClient{}
-}
-
-func (c *TritonClient) Score(ctx context.Context, event *pb.TransactionEvent, features map[string]float64) (float64, error) {
-	return 0.12, nil
-}
+fraud-service/triton_client.go
