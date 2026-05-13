557584e8356ba78ed0c32717d981b9a8d46a27db
PSURI1894 <parthsuri009@gmail.com>
2026-05-13 07:30:00 +0530

    Refine and optimize implementation of feast-offline-iceberg codebase

--- feature-store/entities.py
+++ feature-store/entities.py
@@ -1,5 +1,1 @@
-from feast import Entity
-
-card_id = Entity(name="card_id", value_type=Entity.ValueType.STRING, description="Payment card identifier")
-merchant_id = Entity(name="merchant_id", value_type=Entity.ValueType.STRING, description="Merchant account ID")
-device_id = Entity(name="device_id", value_type=Entity.ValueType.STRING, description="Device fingerprint token")
+feature-store/entities.py
