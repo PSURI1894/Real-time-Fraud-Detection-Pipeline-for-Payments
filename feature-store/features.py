a66ebe451afeae4a940fc8d9a2de11825c00c620
PSURI1894 <parthsuri009@gmail.com>
2026-05-13 15:49:00 +0530

    Refine and optimize implementation of feast-online-redis codebase

--- feature-store/features.py
+++ feature-store/features.py
@@ -1,16 +1,1 @@
-from datetime import timedelta
-from feast import FeatureView, Field
-from feast.types import Float64, Int64
-from entities import card_id, merchant_id
-
-card_features_view = FeatureView(
-    name="card_velocity_features",
-    entities=[card_id],
-    ttl=timedelta(days=30),
-    schema=[
-        Field(name="recent_card_velocity_1m", dtype=Int64),
-        Field(name="card_amount_sum_1h", dtype=Float64),
-    ],
-    online=True,
-    source=None,
-)
+feature-store/features.py
